4b435fe9a4611425380460dc530f273b802a03e3
MurielEsneider <eesneider2118@gmail.com>
2024-12-10 14:41:14 -0500

    1

---
+++ alquilerAutos-backend/controller/clientecontroller.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcrypt');
+const { Cliente } = require('../models');
+
+ //login
+ exports.loginCliente = async (req, res) => {
+  try{
+    const {correo, password} = req.body;
+    // Buscar al cliente por correo
+    const cliente = await Cliente.findOne({ where: {correo}});
+
+    if (!cliente){
+      return res.status(404).json({mensaje: "Cliente no encontrado"});
+      
+    }
+    // Verificar la contraseña
+    const passwordValido =  await bcrypt.compare(password, cliente.password);
+    
+    if (!passwordValido){
+      return res.status(401).json({ mensaje: "Contraseña incorrecta"});
+    }
+
+    // Responder con éxito
+    return res.status(200).json({ mensaje: "Inicio de sesión exitoso", cliente});
+
+  } catch (error) {
+    console.error("Error al inicar sesión: ", error);
+    res.status(500).json({ mensaje: "Error al iniciar sesión", error: error.message});
+  }
+ }
+
+ 
+  ////////////////////////////
+exports.registrarCliente = async (req, res) => {
+  try {
+    const { nombre, correo, numLic, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 es el número de salt rounds
+    const nuevoCliente = await Cliente.create({ nombre, correo, numLic, password: hashedPassword });
+    res.status(201).json(nuevoCliente);
+  } catch (error) {
+    console.error("Error al crear el cliente:", error);
+    res.status(500).json({ mensaje: "Error al crear el cliente", error: error.message });
+  }
+};
+
+exports.verclientes = async (req, res) => {
+  try {
+    const clientes = await Cliente.findAll();
+    res.json(clientes);
+  } catch (error) {
+    console.error("Error al obtener los clientes:", error);
+    res.status(500).json({ mensaje: "Error al obtener los clientes", error: error.message });
+  }
+};
---
+++ alquilerAutos-backend/models/cliente.js
@@ -0,0 +1,56 @@
+'use strict';
+const bcrypt = require("bcrypt");
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Cliente extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Cliente.hasMany(models.Alquiler, { foreignKey: 'clienteId' });
+    }
+  }
+  Cliente.init({
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    correo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true, // Validación de correo electrónico
+      }
+    },
+    numLic: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Cliente',
+    tableName: 'clientes',
+    hooks:{
+      //hook para encriptar la contraseña antes de crear un registro
+      beforeCreate: async (cliente) => {
+        if (cliente.password){
+          const salt = await bcrypct. genSalt(10);
+          cliente.password = await bcrypt.hash(cliente.password, salt);
+
+        }
+      },
+      //hook para encriptar la contraseña antes de actualizar un registro
+      beforeUpdate: async (cliente) => {
+        if (cliente.password && cliente.changed('password')){
+          const salt = await bcrypt.genSalt(10);
+          cliente.password = await bcrypt.hash(cliente.password, salt);
+        }
+      },
+    },
+  });
+  return Cliente;
+};
